0deb35c8fa65f2c9b91e00554504d12697af69f8
seungwuk98 <seungwuk98@snu.ac.kr>
2023-04-17 22:59:24 +0900

    add monkey excution

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"monkey/excute"
 	"monkey/repl"
 	"os"
 	"os/user"
@@ -12,8 +14,17 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
-	fmt.Printf("Feel free to type in commands\n")
-	repl.Start(os.Stdin, os.Stdout)
+	if len(os.Args) == 2 {
+		filename := os.Args[1]
+		code, err := ioutil.ReadFile(filename)
+		if err != nil {
+			panic(err)
+		}
+		input := string(code)
+		excute.ExcuteProgram(input)
+	} else {
+		fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+		fmt.Printf("Feel free to type in commands\n")
+		repl.Start(os.Stdin, os.Stdout)
+	}
 }
